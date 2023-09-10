f2503e758c75488ef959d0d63efa61a83e305cdf
lojin magdy <LojinMagdy19@gmail.com>
2023-09-10 23:36:14 +0300

    loijn part

--- proto-hobbies/AppController.py
+++ proto-hobbies/AppController.py
@@ -8,51 +8,56 @@ database = Database()
 def index():
     return render_template("homepage.html")
 
-@app.route("/protons")
+@app.route("/GetProtons")
 def getProtons():
-    return render_template("protons.html")
+    return render_template("protons.html", proton=database.protons)
 
-@app.route("/hobbies")
+@app.route("/GetHobbies")
 def getHobbies():
-    return render_template (".html")
+    return render_template ("hobby.html" ,hobby=database.hobbies)
 
-@app.route("/protons",methods="POST")
+@app.route("/AppProtons",methods=["POST"])
 def addProton(protonName: str, protonAge: int, protonTrack: str):
         protonName = request.form.get("protonName")
         protonAge = request.form.get("protonAge")
         protonTrack= request.form.get("protonTrack")
         database.addProton(protonName,protonAge,protonTrack)
-        return redirect("/protons")
+        return redirect("/AppProtons")
 
-
-@app.route("/hobbies",methods="POST")
+@app.route("/AddHobbies",methods=["POST"])
 def addHobby(hobbyTitle: str, imagePAth: str):
         hobbyTitle = request.form.get("hobbyTitle")
         imagePAth = request.form.get("imgePAth")
-        database.addProton(hobbyTitle,imagePAth)
-        return redirect("/hobbies")
+        database.addHobby(hobbyTitle,imagePAth)
+        return redirect("/AddHobbies")
 
 
-@app.route("/hobbies/<hobbyTitle>/<protonName>")
-def matchProtoHobby(hobbyTitle: str,protonName: str):
-    #TODO
-    pass
+#@app.route("/hobbies/<hobbyTitle>/<protonName>")
+#def matchProtoHobby(hobbyTitle: str,protonName: str):
+  #TODO
+#    pass
 
 #Views the hobby title, image and protons who like it
-@app.route("/hobbies/<hobbyTitle>")
+@app.route("/ViewHobbies/<hobbyTitle>")
 def viewHobby(hobbyTitle: str):
-    #TODO
-    pass
+    database.findHobby(hobbyTitle)
+    return render_template("homepage.html")
 
-@app.route("/hobbies/<hobbyTitle>",methods="DELETE")
+@app.route("/ViewProtons/<protonName>")
+def viewProton(protonName: str):
+    database.findProton(protonName)
+    return render_template("homepage.html")
+
+@app.route("/DeleteHobbies/<hobbyTitle>",methods=["DELETE"])
 def deleteHobby(hobbyTitle: str):
      hobbyTitle=request.form.get("hobby")
      database.deleteHobby(hobbyTitle)
-     return redirect("/hobbies")
+     return redirect("/DeleteHobbies")
 
-
-@app.route("/protons/<protonName>",methods="DELETE")
+@app.route("/DeleteProtons/<protonName>",methods=["DELETE"])
 def deleteProton(protonName: str):
      protonName=request.form.get("protonName")
      database.deleteProton(protonName)
-     return redirect("/protons")
+     return redirect("/DeleteProtons")
+
+app.run()
